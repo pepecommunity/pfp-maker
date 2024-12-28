8eed0dbb6a28a7dfa6d3c7cb91a288e76ab6d539
pepecommunity <pepecommunityproject@gmail.com>
2024-12-28 15:35:35 -0600

    updated tailwinf contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -6,6 +10,7 @@ export default {
         'portrait-mobile': { 'raw': '(max-width: 480px) and (orientation: portrait)' },
         'landscape-mobile': { 'raw': '(min-width: 481px) and (max-width: 768px) and (orientation: landscape)' },
         'desktop': { 'raw': '(min-width: 769px)' },
+      },
       colors: {
         mainBlue: "#d9d7d7",
         secondaryBlue: "#8f8b8b",
@@ -16,5 +21,4 @@ export default {
       },
     },
   },
-  plugins: [],
 };
