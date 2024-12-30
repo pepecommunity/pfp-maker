e45c3b70a4169534894ad0d8217bb0a4ad4629d9
pepecommunity <pepecommunityproject@gmail.com>
2024-12-29 21:24:54 -0600

    uodated nodes to filt with newly created files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example: Split vendor code into a separate chunk
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit
+  },
 })
